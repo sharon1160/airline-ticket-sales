16e9ccf15de92397e08d742ae437fbd85374eee2
Sharon CR <schullunquia@unsa.edu.pe>
2022-09-30 21:15:58 -0500

    Add classes

---
+++ model/Fly.py
@@ -0,0 +1,11 @@
+from model.Route import Route
+
+
+class Fly(object):
+    def __init__(self, route: Route, num_economic_tickets: int, num_premium_tickets: int):
+        """
+        Constructor de la clase Product
+        """
+        self.route: Route = route
+        self.num_economic_tickets: int = num_economic_tickets
+        self.num_premium_tickets: int = num_premium_tickets
